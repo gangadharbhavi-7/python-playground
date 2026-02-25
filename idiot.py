a59229b28eb554a44e319843e859e424da3555ee
Gangadhar B <gangadharbavi312004@gmail.com>
2026-02-25 22:58:19 +0530

    Update idiot.py

--- idiot.py
+++ idiot.py
@@ -1,6 +1,6 @@
 import pyinputplus as pyip
 while True:
-    prompt='want to know how to keep an idiot busy for hours?\n'
+    prompt="want to know how to keep an idiot busy for hours?\n"
     response=pyip.inputYesNo(prompt)
     if response=='no':
         break
