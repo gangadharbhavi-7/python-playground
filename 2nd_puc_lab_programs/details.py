2439af3c8e519a3721869ad2e0866422d5e8ed00
gangadharbhavi-7 <gangadharbavi312004@gmail.com>
2026-04-01 17:48:47 +0530

    Add details program of lab component

---
+++ 2nd_puc_lab_programs/details.py
@@ -0,0 +1,17 @@
+name=input("Enter Your name: ")
+usn=input("Enter your usn: ")
+marks1=int(input("Enter marks of subject 1: "))
+marks2=int(input("Enter marks of subject 2: "))
+marks3=int(input("Enter marks of subject 3: "))
+total=marks1+marks2+marks3
+percentage=total/3
+print("\n*************STUDENT DETAILS***************\n")
+print("Name: ",name)
+print("USN: ",usn)
+print("Marks of subject 1: ",marks1)
+print("Marks of subject 2: ",marks2)
+print("Marks of subject 3: ",marks3)
+print("Total marks: ",total)
+print("Percentage: ", percentage)
+
+
